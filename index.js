6a74114d9314bbc0fb4f1b24a681fcc838ff784b
Nivash_sharma <nivasmathu@gmail.com>
2023-12-21 12:37:48 +0530

    intilize canvas

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
-console.log("i am in index.js")
+// console.log("i am in index.js")
 
 
-const canvas =new fabric.Canvas("canvas",{
-    width:500,
-    height:500,
+// const canvas =new fabric.Canvas("canvas",{
+//     width:500,
+//     height:500,
     
-});
+// });
  
 
-canvas.renderAll();
+// canvas.renderAll();
 
-fabric.Image.fromURL("https://th.bing.com/th/id/OIP.Z_PIeIRDajXPmZHROt-T_QHaEK?rs=1&pid=ImgDetMain",(img)=>{
-    canvas.backgroundImage= img
-    canvas.renderAll()
-});
+// fabric.Image.fromURL("https://th.bing.com/th/id/OIP.Z_PIeIRDajXPmZHROt-T_QHaEK?rs=1&pid=ImgDetMain",(img)=>{
+//     canvas.backgroundImage= img
+//     canvas.renderAll()
+// });
+
+const initiCanvas = (id)=>{
+    return new fabric.Canvas(id,{
+        width:500,
+        height:500,
+    });
+}
+
+const setBackground = (url,canvas)=> {
+    fabric.Image.fromURL(url,(img)=> {
+        canvas.backgroundImage = img
+        canvas.renderAll()
+    })
+}
+
+const canvas = initiCanvas("canvas");
+
+setBackground("https://th.bing.com/th/id/OIP.Z_PIeIRDajXPmZHROt-T_QHaEK?rs=1&pid=ImgDetMain",canvas);
