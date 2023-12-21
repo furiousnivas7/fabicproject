6b1253883df440f0658cf999315826d34e28e972
Nivash_sharma <nivasmathu@gmail.com>
2023-12-21 12:24:43 +0530

    insert image

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ console.log("i am in index.js")
 const canvas =new fabric.Canvas("canvas",{
     width:500,
     height:500,
-    backgroundColor:"red"
+    
 });
  
 
 canvas.renderAll();
+
+fabric.Image.fromURL("1.png",(img)=>{
+    canvas.backgroundImage= img
+    canvas.renderAll()
+});
