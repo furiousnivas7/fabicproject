a7164480d3ce17055022167c407c85adc255082e
Nivash_sharma <nivasmathu@gmail.com>
2023-12-21 19:18:30 +0530

    setpanevents

--- index.js
+++ index.js
@@ -21,6 +21,33 @@ const togglePan = () =>{
         currentMode = modes.pan
     }
 }
+ 
+const setPanEvents = (canvas) =>{
+    canvas.on("mouse:over",(event)=>{
+        if (mousePressed && currentMode === modes.pan){
+            canvas.setCursor("grab")
+            canvas.renderAll()
+            const mEvent = event.e;
+            const delta = new fabric.Point(mEvent.movementX,mEvent.movementY)
+            canvas.relativePan(delta)
+        }
+    })
+    
+    canvas.on("mouse:down",(event)=>{
+        mousePressed = true;
+        if (currentMode === modes.pan){
+            canvas.setCursor("grab")
+            canvas.renderAll()
+        }
+    })
+    
+    canvas.on("mouse:up",(event)=>{
+        mousePressed = false
+        canvas.setCursor("default")
+        canvas.renderAll()
+    })
+
+}
 
 const canvas = initiCanvas("canvas");
 let mousePressed = false;
@@ -32,27 +59,5 @@ const modes={
 
 setBackground("https://th.bing.com/th/id/OIP.Z_PIeIRDajXPmZHROt-T_QHaEK?rs=1&pid=ImgDetMain",canvas);
 
+setPanEvents(canvas);
 
-canvas.on("mouse:over",(event)=>{
-    if (mousePressed && currentMode === modes.pan){
-        canvas.setCursor("grab")
-        canvas.renderAll()
-        const mEvent = event.e;
-        const delta = new fabric.Point(mEvent.movementX,mEvent.movementY)
-        canvas.relativePan(delta)
-    }
-})
-
-canvas.on("mouse:down",(event)=>{
-    mousePressed = true;
-    if (currentMode === modes.pan){
-        canvas.setCursor("grab")
-        canvas.renderAll()
-    }
-})
-
-canvas.on("mouse:up",(event)=>{
-    mousePressed = false
-    canvas.setCursor("default")
-    canvas.renderAll()
-})
