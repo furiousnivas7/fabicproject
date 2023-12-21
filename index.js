28570d3c238ba08c8e3acef2e87f57f4ec121c88
Nivash_sharma <nivasmathu@gmail.com>
2023-12-21 13:29:43 +0530

    update moveover

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-// console.log("i am in index.js")
-
-
-// const canvas =new fabric.Canvas("canvas",{
-//     width:500,
-//     height:500,
-    
-// });
- 
-
-// canvas.renderAll();
-
-// fabric.Image.fromURL("https://th.bing.com/th/id/OIP.Z_PIeIRDajXPmZHROt-T_QHaEK?rs=1&pid=ImgDetMain",(img)=>{
-//     canvas.backgroundImage= img
-//     canvas.renderAll()
-// });
 
 const initiCanvas = (id)=>{
     return new fabric.Canvas(id,{
@@ -34,6 +18,8 @@ const canvas = initiCanvas("canvas");
 setBackground("https://th.bing.com/th/id/OIP.Z_PIeIRDajXPmZHROt-T_QHaEK?rs=1&pid=ImgDetMain",canvas);
 
 
-canvas.on("mouse:over",(e)=>{
-    console.log(e)
+canvas.on("mouse:over",(event)=>{
+    const mEvent = event.e;
+    const delta = new fabric.Point(mEvent.movementX,mEvent.movementY)
+    canvas.relativePan(delta)
 })
